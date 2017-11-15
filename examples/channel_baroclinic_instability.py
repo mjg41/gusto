d56c3ce9a7d4e0a118b1956010e90c14ac809540
Jemma Shipton <jshipton@ma-jshipton.ma.ic.ac.uk>
2017-11-15 16:56:40 +0000

    updating examples - neither currently working

--- examples/channel_baroclinic_instability.py
+++ examples/channel_baroclinic_instability.py
@@ -1,12 +1,12 @@
 from gusto import *
-from firedrake import SpatialCoordinate, sin, pi, cos, exp, ln, Function, solve, TestFunction, TrialFunction, dx, File
+from firedrake import SpatialCoordinate, sin, pi, cos, exp, ln, Function, solve, TestFunction, TrialFunction, dx
 import sys
 
-dt = 0.1
+dt = 10.
 if '--running-tests' in sys.argv:
     tmax = dt
 else:
-    tmax = 3600.
+    tmax = 15*24*60*60.
 
 # set up domain
 delta_z = 1.e3  # 1km vertical grid spacing
@@ -15,10 +15,13 @@ H = 3.e4  # height of domain 30km
 delta_x = 1.e5  # 100km horizontal grid spacing
 L = 4.e7  # zonal extent of domain 40000km
 W = 6.e6  # meridional extent of domain 6000km
-columns = (int(L/delta_x), int(W/delta_x))  # number of columns
-f0 = 1.e-4
-beta = 2.e-11
-domain = ChannelDomain(L, W, H, columns, nlayers, coriolis=(f0, beta))
+nx = int(L/delta_x)
+ny = int(W/delta_x)
+
+parameters = CompressibleParameters()
+domain = ChannelDomain(parameters=parameters,
+                       nx=nx, ny=ny, nlayers=nlayers, L=L, W=W, H=H,
+                       rotation_option="beta_plane")
 
 fieldlist = ['u', 'rho', 'theta']
 
@@ -26,14 +29,12 @@ timestepping = TimesteppingParameters(dt=dt)
 output = OutputParameters(dirname='channel_baroclinic_instability', dumpfreq=1,
                           perturbation_fields=['rho', 'theta'])
 diagnostic_fields = [CourantNumber()]
-parameters = CompressibleParameters()
 
 state = State(domain,
               vertical_degree=1, horizontal_degree=1,
               family="RTCF",
               timestepping=timestepping,
               output=output,
-              parameters=parameters,
               fieldlist=fieldlist,
               diagnostic_fields=diagnostic_fields)
 
@@ -56,47 +57,58 @@ p_s = p_0
 c_p = parameters.cp
 R_d = parameters.R_d
 kappa = parameters.kappa
+f0 = parameters.f0
+beta = parameters.beta
 
 x, y, z = SpatialCoordinate(domain.mesh)
 
 Vdg = state.spaces("DG")
 eta = Function(Vdg).assign(1.e-7)
-u0 = 35.
+u_0 = 35.
 T0 = 288
 Gamma = 0.005
 b = 2.
 y0 = W/2.
 
+
 def Phi_bar(eta):
     return T0*g/Gamma*(1 - eta**(R_d*Gamma/g))
+
+
 def Phi_prime(eta):
-    return 0.5*u0*((f0 - beta*y0)*(y - 0.5*W*(1 + sin(2*pi*y/W)/pi)) + 0.5*beta*(y**2 - W*(y*sin(2*pi*y/W)/pi + 0.5*W/(pi**2)*cos(2*pi*y/W) - W/3. - 0.5*W/(pi**2))))
+    return 0.5*u_0*((f0 - beta*y0)*(y - 0.5*W*(1 + sin(2*pi*y/W)/pi)) + 0.5*beta*(y**2 - W*(y*sin(2*pi*y/W)/pi + 0.5*W/(pi**2)*cos(2*pi*y/W) - W/3. - 0.5*W/(pi**2))))
+
+
 def Phi(eta):
     return Phi_bar(eta) + Phi_prime(eta)*ln(eta)*exp(-(ln(eta)/b)**2)
 
+
 def T_bar(eta):
     return T0*eta**(R_d*Gamma/g)
+
+
 def T_prime(eta):
     return Phi_prime(eta)/R_d*(2/(b**2)*(ln(eta)**2) - 1)*exp(-(ln(eta)/b)**2)
+
+
 def T(eta):
     return T_bar(eta) + T_prime(eta)
+
 
 v = TestFunction(Vdg)
 tri = TrialFunction(Vdg)
 F = (-g*z + Phi(eta))*v*dx(degree=8)
 solve(F == 0, eta)
 
-eta_file = File('eta.pvd')
-eta_file.write(eta)
-
-p = p_0* eta
+p = p_0*eta
 rho_b = Function(Vr).interpolate(p/(R_d*T(eta)))
 theta_b = Function(Vt).interpolate(T(eta)*pow(p_0/p, R_d/c_p))
 
 # Calculate hydrostatic Pi
 compressible_hydrostatic_balance(state, theta_b, rho_b, solve_for_rho=True)
+theta0.assign(theta_b)
 rho0.assign(rho_b)
-u = -u0*sin(pi*y/W)**2*ln(eta)*eta**(-ln(eta)/b**2)
+u = -u_0*sin(pi*y/W)**2*ln(eta)*eta**(-ln(eta)/b**2)
 u0.project(as_vector([u, 0.0, 0.0]))
 
 state.initialise([('u', u0),
--- examples/dcmip_3-1_meanflow_quads.py
+++ examples/dcmip_3-1_meanflow_quads.py
@@ -1,31 +1,24 @@
 from gusto import *
 from firedrake import CubedSphereMesh, ExtrudedMesh, Expression, \
-    VectorFunctionSpace
+    VectorFunctionSpace, SpatialCoordinate, as_vector, FunctionSpace, Function
 from firedrake import exp, acos, cos, sin
 import numpy as np
+import sys
 
-nlayers = 20  # Number of horizontal layers (was 20)
-refinements = 4  # number of horizontal cells = 20*(4^refinements) (was 5)
+dt = 10.
+if '--running-tests' in sys.argv:
+    tmax = dt
+    nlayers = 4  # Number of horizontal layers (was 20)
+    refinements = 2  # number of horizontal cells = 20*(4^refinements) (was 5)
+else:
+    tmax = 100.
+    nlayers = 20  # Number of horizontal layers (was 20)
+    refinements = 4  # number of horizontal cells = 20*(4^refinements) (was 5)
 
 # build surface mesh
 a_ref = 6.37122e6
 X = 125.0  # Reduced-size Earth reduction factor
 a = a_ref/X
-g = 9.810616
-N = 0.01  # Brunt-Vaisala frequency (1/s)
-p_0 = 1000.0 * 100.0  # Reference pressure (Pa, not hPa)
-c_p = 1004.5  # SHC of dry air at constant pressure (J/kg/K)
-R_d = 287.0  # Gas constant for dry air (J/kg/K)
-kappa = 2.0/7.0  # R_d/c_p
-T_eq = 300.0  # Isothermal atmospheric temperature (K)
-p_eq = 1000.0 * 100.0  # Reference surface pressure at the equator
-u_0 = 20.0  # Maximum amplitude of the zonal wind (m/s)
-
-d = 5000.0  # Width parameter for Theta'
-lamda_c = 2.0*np.pi/3.0  # Longitudinal centerpoint of Theta'
-phi_c = 0.0  # Latitudinal centerpoint of Theta' (equator)
-deltaTheta = 1.0  # Maximum amplitude of Theta' (K)
-L_z = 20000.0  # Vertical wave length of the Theta' perturbation
 
 m = CubedSphereMesh(radius=a,
                     refinement_level=refinements,
@@ -36,28 +29,28 @@ z_top = 1.0e4  # Height position of the model top
 mesh = ExtrudedMesh(m, layers=nlayers, layer_height=z_top/nlayers,
                     extrusion_type="radial")
 
-domain = SphericalDomain(mesh=mesh, is_rotating=False)
+parameters = CompressibleParameters()
+domain = SphericalDomain(mesh=mesh, parameters=parameters)
 
 # Space for initialising velocity (using this ensures things are in layers)
 W_VectorCG1 = VectorFunctionSpace(mesh, "CG", 1)
 W_CG1 = FunctionSpace(mesh, "CG", 1)
 
 # Create polar coordinates
-z = Function(W_CG1).interpolate(Expression("sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]) - a",a=a))  # Since we use a CG1 field, this is constant on layers
+z = Function(W_CG1).interpolate(Expression("sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]) - a", a=a))  # Since we use a CG1 field, this is constant on layers
 lat = Function(W_CG1).interpolate(Expression("asin(x[2]/sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]))"))
 lon = Function(W_CG1).interpolate(Expression("atan2(x[1], x[0])"))
 
-fieldlist = ['u','rho','theta']
-timestepping = TimesteppingParameters(dt=10.0)
-output = OutputParameters(Verbose=True, dumpfreq=100000, dirname='meanflow_ref',
+fieldlist = ['u', 'rho', 'theta']
+timestepping = TimesteppingParameters(dt=dt)
+output = OutputParameters(Verbose=True, dumpfreq=1, dirname='meanflow_ref',
                           perturbation_fields=['theta', 'rho'])
-parameters = CompressibleParameters()
 
-state = State(mesh, vertical_degree=1, horizontal_degree=1,
+state = State(domain,
+              vertical_degree=1, horizontal_degree=1,
               family="RTCF",
               timestepping=timestepping,
               output=output,
-              parameters=parameters,
               fieldlist=fieldlist)
 
 # Initial conditions
@@ -71,19 +64,33 @@ Vt = theta0.function_space()
 Vr = rho0.function_space()
 
 # Initial conditions with u0
-#u = u0 * cos(lat)
+# u = u0 * cos(lat)
 x = SpatialCoordinate(mesh)
 u_max = 20.
+g = parameters.g
+N = parameters.N  # Brunt-Vaisala frequency (1/s)
+p_0 = parameters.p_0  # Reference pressure (Pa, not hPa)
+c_p = parameters.cp  # SHC of dry air at constant pressure (J/kg/K)
+R_d = parameters.R_d  # Gas constant for dry air (J/kg/K)
+kappa = parameters.kappa  # R_d/c_p
+T_eq = 300.0  # Isothermal atmospheric temperature (K)
+p_eq = 1000.0 * 100.0  # Reference surface pressure at the equator
+u_0 = 20.0  # Maximum amplitude of the zonal wind (m/s)
+
+d = 5000.0  # Width parameter for Theta'
+lamda_c = 2.0*np.pi/3.0  # Longitudinal centerpoint of Theta'
+phi_c = 0.0  # Latitudinal centerpoint of Theta' (equator)
+deltaTheta = 1.0  # Maximum amplitude of Theta' (K)
+L_z = 20000.0  # Vertical wave length of the Theta' perturbation
 uexpr = as_vector([-u_max*x[1]/a, u_max*x[0]/a, 0.0])
 u0.project(uexpr)
 # Surface temperature
 G = g**2/(N**2*c_p)
-#TS = bigG + (Teq-bigG)*exp( -(u0*N2/(4.d0*g*g))*(u0+2.d0*om*as)*(cos(2.d0*lat)-1.d0)    )
-Ts = Function(W_CG1).interpolate(G + (T_eq-G)*exp( -(u_max*N**2/(4*g*g))*u_max*(cos(2.0*lat)-1.0)))
+# TS = bigG + (Teq-bigG)*exp( -(u0*N2/(4.d0*g*g))*(u0+2.d0*om*as)*(cos(2.d0*lat)-1.d0)    )
+Ts = Function(W_CG1).interpolate(G + (T_eq-G)*exp(-(u_max*N**2/(4*g*g))*u_max*(cos(2.0*lat)-1.0)))
 
 # surface pressure
-#ps = peq*exp( (u0/(4.0*bigG*Rd))*(u0+2.0*Om*as)*(cos(2.0*lat)-1.0)  ) &
-#		            * (TS/Teq)**(cp/Rd)
+# ps = peq*exp( (u0/(4.0*bigG*Rd))*(u0+2.0*Om*as)*(cos(2.0*lat)-1.0) ) * (TS/Teq)**(cp/Rd)
 psexp = p_eq*exp((u_max/(4.0*G*R_d))*u_max*(cos(2.0*lat)-1.0))*(Ts/T_eq)**(1.0/kappa)
 
 ps = Function(W_CG1).interpolate(psexp)
@@ -124,20 +131,22 @@ theta0.interpolate(theta_pert)
 theta0 += theta_b
 rho0.assign(rho_b)
 
-state.initialise({'u': u0, 'rho': rho0, 'theta': theta0})
-state.set_reference_profiles({'rho': rho_b, 'theta': theta_b})
+state.initialise([('u', u0),
+                  ('rho', rho0),
+                  ('theta', theta0)])
+state.set_reference_profiles([('rho', rho_b),
+                              ('theta', theta_b)])
 
 # Set up advection schemes
 ueqn = EulerPoincare(state, Vu)
 rhoeqn = AdvectionEquation(state, Vr, equation_form="continuity")
 thetaeqn = EmbeddedDGAdvection(state, Vt,
                                equation_form="advective")
-advection_dict = {}
-advection_dict["u"] = ThetaMethod(state, u0, ueqn)
-advection_dict["rho"] = SSPRK3(state, rho0, rhoeqn)
-advection_dict["theta"] = SSPRK3(state, theta0, thetaeqn)
+advected_fields = []
+advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
+advected_fields.append(("rho", SSPRK3(state, rho0, rhoeqn)))
+advected_fields.append(("theta", SSPRK3(state, theta0, thetaeqn)))
 
-# Set up linear solver
 # Set up linear solver
 schur_params = {'pc_type': 'fieldsplit',
                 'pc_fieldsplit_type': 'schur',
@@ -160,13 +169,13 @@ schur_params = {'pc_type': 'fieldsplit',
                 'fieldsplit_1_mg_levels_ksp_max_it': 5,
                 'fieldsplit_1_mg_levels_pc_type': 'bjacobi',
                 'fieldsplit_1_mg_levels_sub_pc_type': 'ilu'}
-linear_solver = CompressibleSolver(state, params=schur_params)
+linear_solver = CompressibleSolver(state)
 
 # Set up forcing
 compressible_forcing = CompressibleForcing(state)
 
 # build time stepper
-stepper = CrankNicolson(state, advection_dict, linear_solver,
+stepper = CrankNicolson(state, advected_fields, linear_solver,
                         compressible_forcing)
 
-stepper.run(t=0, tmax=100.0) #tmax was 3600.
+stepper.run(t=0, tmax=tmax)  # tmax was 3600.
